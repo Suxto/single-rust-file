0022f4429ac4db8ced36ddbaf16360e4842ca19e
Suxto <suxton@icloud.com>
2023-09-10 16:08:11 +0800

    ver 0.0.1

---
+++ README.md
@@ -0,0 +1,23 @@
+# single-rust-file
+
+This extension allows you to add a `.rs` file as a standalone executable file, this extension can be used with [Cargo Scripts](https://marketplace.visualstudio.com/items?itemName=taiyuuki.vscode-cargo-scripts "link").
+
+## Features
+
+Just help you add file description in `Cargo.toml`
+
+## Requirements
+
+Cargo
+
+[Cargo Scripts](https://marketplace.visualstudio.com/items?itemName=taiyuuki.vscode-cargo-scripts "link").(Optional)
+
+## Known Issues
+
+I would fix the Issues as long as I am not busy.
+
++ You may add a file twice by accident, which would make cargo cannot run your file. Just remove the extra item in `Cargo.toml` manually :(
+
+## Release Notes
+
++ 2023-9-10 **First Release** Ver 0.0.1
---
+++ extension.js
@@ -0,0 +1,91 @@
+const vscode = require('vscode');
+const path = require('path');
+const { parse, stringify } = require('@iarna/toml');
+const fs = require('fs');
+
+async function verifyCargoToml(folderPath) {
+	const cargoTomlPath = path.join(folderPath, 'Cargo.toml');
+	try {
+		await vscode.workspace.fs.stat(vscode.Uri.file(cargoTomlPath));
+		return cargoTomlPath;
+	} catch (error) {
+		return null;
+	}
+}
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with  registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('single-rust-file.addToTOML', async (uri) => {
+		if (!uri || !uri.fsPath.endsWith('.rs')) return;
+		let currentFolderPath = path.dirname(uri.fsPath);
+
+		let currentFileName = path.basename(uri.fsPath).split('.');
+
+		let cargoTomlPath = await verifyCargoToml(currentFolderPath);
+
+		while (!cargoTomlPath && currentFolderPath !== '/') {
+			const parentFolderPath = path.dirname(currentFolderPath);
+			cargoTomlPath = await verifyCargoToml(parentFolderPath);
+			currentFolderPath = parentFolderPath;
+		}
+
+		// console.log(cargoTomlPath);
+		if (!cargoTomlPath) {
+			vscode.window.showErrorMessage('Can`t find Cargo.toml');
+			return;
+		}
+		let relativePath = uri.fsPath.replace(path.dirname(cargoTomlPath), '').substring(1);
+		relativePath = relativePath.replace(/\\/g, "/");
+
+		const config = {
+			name: currentFileName[0],
+			path: relativePath
+		};
+
+		// let binConfig = [config];
+		// console.log(binSection);
+
+		try {
+			const cargoTomlData = fs.readFileSync(cargoTomlPath, 'utf-8');
+			const cargoTomlObject = parse(cargoTomlData);
+			// console.log(cargoTomlObject);
+			if (Array.isArray(cargoTomlObject.bin)) {
+				// @ts-ignore
+				cargoTomlObject.bin.push(config);
+			} else if (cargoTomlObject.package) {
+				cargoTomlObject.bin = [config];
+			} else {
+				vscode.window.showErrorMessage(`No [package] section found in ${cargoTomlPath}. Please check the toml file.`);
+				return;
+			}
+			let scriptName = 'run_' + currentFileName[0];
+			let scriptAction = "cargo run --bin " + currentFileName[0];
+
+			cargoTomlObject.package['metadata']['scripts'][scriptName] = scriptAction;
+
+			console.log(cargoTomlObject);
+
+			const updatedCargoTomlData = stringify(cargoTomlObject);
+			fs.writeFileSync(cargoTomlPath, updatedCargoTomlData, 'utf-8');
+
+			vscode.window.showInformationMessage(`Content inserted to ${cargoTomlPath}.`);
+		} catch (error) {
+			vscode.window.showErrorMessage(`Failed to insert content to ${cargoTomlPath}: ${error}`);
+		}
+
+	});
+	context.subscriptions.push(disposable);
+}
+
+// This method is called when your extension is deactivated
+function deactivate() { }
+
+module.exports = {
+	activate,
+	deactivate
+}
+
